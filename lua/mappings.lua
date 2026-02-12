e43d3c2de7bc84ad2dce8788df7157949931b21e
Adityya-K <adityyakaushal@gmail.com>
2026-02-12 01:33:43 -0500

    feat: added diagnostic toggle

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,4 +8,13 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map({"n", "v"}, "<leader>uw", ":set wrap!<CR>", { desc = "Toggle word wrap" })
 map({"n", "v"}, "<leader>us", ":set spell!<CR>", { desc = "Toggle spelling" })
 
+function Toggle_diagnostics()
+    if vim.diagnostic.is_enabled then
+        vim.diagnostic.enable(false)
+    else
+        vim.diagnostic.enable(true)
+    end
+end
+map("", "<leader>ud", Toggle_diagnostics, {desc = "Toggle diagnostics"} )
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
