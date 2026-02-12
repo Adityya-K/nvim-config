8bc26c8f579d2419a1abcf43d0af5765ca093fae
Adityya-K <adityyakaushal@gmail.com>
2026-02-12 01:41:06 -0500

    fix: diagnostics toggle

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,13 +8,11 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map({"n", "v"}, "<leader>uw", ":set wrap!<CR>", { desc = "Toggle word wrap" })
 map({"n", "v"}, "<leader>us", ":set spell!<CR>", { desc = "Toggle spelling" })
 
-function Toggle_diagnostics()
-    if vim.diagnostic.is_enabled then
-        vim.diagnostic.enable(false)
-    else
-        vim.diagnostic.enable(true)
-    end
-end
-map("", "<leader>ud", Toggle_diagnostics, {desc = "Toggle diagnostics"} )
+map("n", "<leader>ud", function()
+  local is_enabled = vim.diagnostic.is_enabled()
+  vim.diagnostic.enable(not is_enabled)
+  print("Diagnostics " .. (is_enabled and "Disabled" or "Enabled"))
+end, { desc = "Toggle Diagnostics" })
+
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
